df7807acfac8a8d7ebd7c5cd9c53a22fe2c639e9
unknown <1040185058@qq.com>
2019-07-15 22:04:15 +0800

    add colorful points

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 * 依赖 canvas-sketch 和 canvas-sketch-cli
 
 ## 记录
-* [生成一个随机网格圆点](./src/grid.js)
+* [生成一个网格圆点](./src/grid.js)
+* [随机点阵](./src/grid-random.js)
+* [随机位置/颜色/大小点图](./src/grid-picture.js)
--- src/grid-picture.js
+++ src/grid-picture.js
@@ -1,8 +1,8 @@
 /**
-* 生成一个随机分布的网格点
-* 网格点分布距离边界固定margin
+* 生成一个位置，颜色，大小随机的点分布图
 */
 const canvasSketch = require('canvas-sketch')
+const { randomColor } = require('./utils')
 
 const settings = {
   dimensions: [ 2048, 2048 ]
@@ -15,13 +15,18 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = x / (count - 1) // 归一化坐标
         const v = y / (count - 1)
-        points.push([u, v])
+        const position = [u, v]
+        points.push({
+          color: randomColor(),
+          radius: Math.random() * 20,
+          position
+        })
       }
     }
     return points
   }
 
-  const points = createGrid(30).filter(() => Math.random() > 0.5)
+  const points = createGrid(60).filter(() => Math.random() > 0.5)
   
   return ({ context, width, height }) => {
     const margin = width * 0.175
@@ -29,13 +34,19 @@ const sketch = () => {
     context.fillStyle = 'white'
     context.fillRect(0, 0, width, height)
 
-    points.forEach(([u, v]) => {
+    points.forEach(data => {
+      const {
+        position,
+        radius,
+        color
+      } = data
+      const [u, v] = position
       const x = u * (width - 2 * margin) + margin  // 限制坐标在 margin 边界范围内
       const y = v * (height - 2 * margin) + margin
 
       context.beginPath()
-      context.arc(x, y, 10, 0, Math.PI * 2) // x y 弧心坐标 radius 半径 startAngle endAngle
-      context.fillStyle = 'black'
+      context.arc(x, y, radius, 0, Math.PI * 2) // x y 弧心坐标 radius 半径 startAngle endAngle
+      context.fillStyle = color
       context.fill()
     })
   }
